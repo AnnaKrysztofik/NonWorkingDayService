2c44c368b48e10d7def421bba67bd7e3d9446e28
Anna Krysztofik <krysztofikanna@gmail.com>
2020-08-19 16:16:54 +0200

    working version SWAGGER

--- src/main/java/api/nonWorkingDays/security/jwt/JwtUtil.java
+++ src/main/java/api/nonWorkingDays/security/jwt/JwtUtil.java
@@ -63,7 +63,7 @@ public class JwtUtil {
         return Jwts.builder()
                 .setClaims(claims)
                 .setSubject(subject)
-             //   .setSubject(role)
+             //  .setSubject(role)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
